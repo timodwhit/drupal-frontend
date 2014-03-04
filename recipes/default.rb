299228fabc2899edb5e539d661d5fb3006b42ddb
Tim Whitney <tim.d.whitney@gmail.com>
2014-03-04 11:30:48 -0700

    Changed structure

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,44 +20,35 @@
 # Install/configure tools for frontend
 
 # For each site, run the following commands
-node[:drupal][:sites].each do |site_name, site|
-  # Only run if the site is active, else no need to compile.
-  if site[:active]
-    #Use a CSS Preprocessor
-    unless site[:drupal_frontend].nil?
-      unless site[:drupal_frontend][:default].nil?
-        unless site[:drupal_frontend][:default][:css_preprocessor].nil?
-          csspre = site[:drupal_frontend][:default][:css_preprocessor]
-          release_path = node[:drupal][:server][:base] + '/' + site_name
-          Chef::Log.debug('drupal-frontend::default: before_restart: site[:drupal-frontend][:default][:css_preprocessor]' + csspre.inspect)
-          # Install each gem
-          Chef::Log.debug('drupal-frontend::default: before_restart: site[:drupal-frontend][:default][:css_preprocessor][:gems]' + csspre[:gems].inspect)
-          csspre[:gems].each do |g|
-            gem_package g do
-              not_if 'gem list | grep #{g}'
-              action :install
-            end
-          end
-          cmd = ""
-          # This allows for the commands to be ran in a single string
-          last = csspre[:commands].last
-          Chef::Log.debug('Last '+ last)
-          csspre[:commands].each do |c|
-            if c == last
-              cmd << c
-            else
-              cmd << c + '; '
-            end
-          end
-          bash 'compile CSS' do
-            user 'root'
-            cwd release_path + '/current/' + csspre[:location]
-            code <<-EOH
-              #{cmd}
-            EOH
-          end
-        end
-      end
+node[:drupal_frontend][:css_preprocessor].each do |site_name, site|
+  #Use a CSS Preprocessor
+  csspre = site
+  location = site_name
+  Chef::Log.debug('drupal-frontend::default: before_restart: site[:drupal-frontend][:default][:css_preprocessor]' + csspre.inspect)
+  # Install each gem
+  Chef::Log.debug('drupal-frontend::default: before_restart: site[:drupal-frontend][:default][:css_preprocessor][:gems]' + csspre[:gems].inspect)
+  csspre[:gems].each do |g|
+    gem_package g do
+      not_if 'gem list | grep #{g}'
+      action :install
     end
+  end
+  cmd = ""
+  # This allows for the commands to be ran in a single string
+  last = csspre[:commands].last
+  Chef::Log.debug('Last '+ last)
+  csspre[:commands].each do |c|
+    if c == last
+      cmd << c
+    else
+      cmd << c + '; '
+    end
+  end
+  bash 'compile CSS' do
+    user 'root'
+    cwd location
+    code <<-EOH
+      #{cmd}
+    EOH
   end
 end
