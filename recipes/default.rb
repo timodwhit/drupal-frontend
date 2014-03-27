1f016727a3bc8bbc7048677eaa1715ab65b5aeda
Tim Whitney <tim.d.whitney@gmail.com>
2014-03-27 12:30:44 -0600

    Adjusts the cookbook to be safer if installed but not used.

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,35 +20,39 @@
 # Install/configure tools for frontend
 
 # For each site, run the following commands
-node[:drupal_frontend][:css_preprocessor].each do |site_name, site|
-  #Use a CSS Preprocessor
-  csspre = site
-  location = site_name
-  Chef::Log.debug('drupal-frontend::default: node[:drupal-frontend][:css_preprocessor]' + csspre.inspect)
-  # Install each gem
-  Chef::Log.debug('drupal-frontend::default: node[:drupal-frontend][:css_preprocessor][:gems]' + csspre[:gems].inspect)
-  csspre[:gems].each do |g|
-    gem_package g do
-      not_if 'gem list | grep #{g}'
-      action :install
+unless node[:drupal_frontend].nil?
+  unless node[:drupal_frontend][:css_preprocessor].nil?
+    node[:drupal_frontend][:css_preprocessor].each do |site_name, site|
+      #Use a CSS Preprocessor
+      csspre = site
+      location = site_name
+      Chef::Log.debug('drupal-frontend::default: node[:drupal-frontend][:css_preprocessor]' + csspre.inspect)
+      # Install each gem
+      Chef::Log.debug('drupal-frontend::default: node[:drupal-frontend][:css_preprocessor][:gems]' + csspre[:gems].inspect)
+      csspre[:gems].each do |g|
+        gem_package g do
+          not_if 'gem list | grep #{g}'
+          action :install
+        end
+      end
+      cmd = ""
+      # This allows for the commands to be ran in a single string
+      last = csspre[:commands].last
+      Chef::Log.debug('Last '+ last)
+      csspre[:commands].each do |c|
+        if c == last
+          cmd << c
+        else
+          cmd << c + '; '
+        end
+      end
+      bash 'compile CSS' do
+        user 'root'
+        cwd location
+        code <<-EOH
+          #{cmd}
+        EOH
+      end
     end
-  end
-  cmd = ""
-  # This allows for the commands to be ran in a single string
-  last = csspre[:commands].last
-  Chef::Log.debug('Last '+ last)
-  csspre[:commands].each do |c|
-    if c == last
-      cmd << c
-    else
-      cmd << c + '; '
-    end
-  end
-  bash 'compile CSS' do
-    user 'root'
-    cwd location
-    code <<-EOH
-      #{cmd}
-    EOH
   end
 end
