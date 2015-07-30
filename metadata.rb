b70aabe0c5af7c2c40ea8ad638d6fb4a2a3dd05d
Tim Whitney <tim.d.whitney@gmail.com>
2015-07-30 17:28:56 -0600

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Tim Whitney"
 maintainer_email "tim.d.whitney@gmail.com"
 license          "Apache 2.0"
 description      "Installs/configures frontend tools"
-version          "0.1.0"
+version          "0.2.0"
 recipe           "Installs/configures nodejs, bundler, bower, grunt"
 
 depends 'nodejs' '~>1.3.0'
